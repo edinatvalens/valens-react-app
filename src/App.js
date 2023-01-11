21a0e1d45a19f403f22da151466015f96c173e74
MedisaSatara <medisa.satara@edu.fit.ba>
2023-01-11 16:41:45 +0100

    changed height size

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import styled from 'styled-components'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import NavBar from './NavBar'
-import Home from './Home'
-import Video from './Video'
-import NotFound from './NotFound'
+import NavBar from '../src/components/NavBar'
+import Home from '../src/components/Home'
+import Video from '../src/components/Video'
+import NotFound from '../src/components/NotFound'
 
 const StyledApp = styled.div`
     text-align: center;
     width: 100%;
-    height: 600px;
+    height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
 `
-
 function App() {
     return (
         <StyledApp>
